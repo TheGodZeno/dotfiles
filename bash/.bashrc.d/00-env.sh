374d048bbd99504e803b554d6dddc958656743c5
TheGodZeno <shugel2004@gmail.com>
2026-04-08 02:06:06 +0300

    feat: add env stuff for bash

---
+++ bash/.bashrc.d/00-env.sh
@@ -0,0 +1,17 @@
+# environment variables
+
+export EDITOR="nvim"
+
+# Pager
+export PAGER="less"
+export LESS="-R"
+
+# History size
+export HISTSIZE=10000
+export HISTFILESIZE=20000
+
+# History behavior
+export HISTORYCONTROL=ignoreboth:erasedups
+
+# Append insted of overwrite
+shopt -s histappend
