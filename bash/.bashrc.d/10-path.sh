3b9ea9c2d8fe5c62a9ead306c491a1efd6a9a3b1
TheGodZeno <shugel2004@gmail.com>
2026-04-08 03:57:02 +0300

    refactor: change indent of case handler

--- bash/.bashrc.d/10-path.sh
+++ bash/.bashrc.d/10-path.sh
@@ -1,16 +1,16 @@
 # User specific environment
 path_prepend() {
-    case ":$PATH:" in
-        *":$1:"*) ;;
-        *) PATH="$1:$PATH" ;;
-    esac
+  case ":$PATH:" in
+  *":$1:"*) ;;
+  *) PATH="$1:$PATH" ;;
+  esac
 }
 
 path_append() {
-    case ":$PATH:" in
-        *":$1:"*) ;;
-        *) PATH="$PATH:$1" ;;
-    esac
+  case ":$PATH:" in
+  *":$1:"*) ;;
+  *) PATH="$PATH:$1" ;;
+  esac
 }
 
 path_prepend "$HOME/.local/bin"
